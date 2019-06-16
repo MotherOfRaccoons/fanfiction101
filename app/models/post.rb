e2d1bb5ef77bd89e651bbf2b71997de83baaa2e2
motherofraccoons <mr.bronium@gmail.com>
2019-06-16 14:12:35 +0300

    added editing, deletion and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  has_many :comments
   validates :title, presence: true,
-                    length: {minimum: 5}
+                    length: { minimum: 5, maximum: 50 }
+  validates :synopsis, presence: true,
+                       length: { minimum: 20, maximum: 500 }
 end
